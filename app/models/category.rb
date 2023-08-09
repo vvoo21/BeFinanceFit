25a4b75d13365558edc82f99113f7cdb61042f35
vvoo21 <vaneolipa@gmail.com>
2023-08-09 17:11:36 -0500

    Fix error

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :transactions
 
-  validates :name, presence: true, length: { in 2..50 }
+  validates :name, presence: true, length: { in: 2..50 }
   validates :icon, presence: true
 end
