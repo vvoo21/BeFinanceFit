8f6d6fa0903f091049e46424ac0d2e2a54e83cc2
vvoo21 <vaneolipa@gmail.com>
2023-08-09 16:28:04 -0500

    rails generate migration AddUserIdToCategories

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :categories, dependent: :destroy
-  has_many :transactios, dependent: :destroy
+  has_many :transactions, dependent: :destroy
 
   validates :name, presence: true, length: { in: 2..50 }
 end
