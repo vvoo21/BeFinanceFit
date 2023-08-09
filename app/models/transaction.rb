3f7abcfedc3f21b1573b53dacc309bac3ba1ded3
vvoo21 <vaneolipa@gmail.com>
2023-08-09 14:41:08 -0500

    Associations and validations: User and Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,9 @@
 class Transaction < ApplicationRecord
+  belongs_to :user
+  has_and_belongs_to_many : categories
+
+  attribute :amount, default: 0
+
+  validates :name, presence: true, length: { in: 2..50 }
+  validate :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :categories, dependent: :destroy
+  has_many :transactios, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 2..50 }
 end
